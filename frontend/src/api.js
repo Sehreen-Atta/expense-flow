907365f372075da8f369efeb14c528dbff74aa60
Sehreen <Sehreenatta@gmail.com>
2026-07-25 17:51:03 +0500

    Fix duplicated API path bug

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://expense-flow-coral-nine.vercel.app/api/expenses',
+  baseURL: 'https://expense-flow-coral-nine.vercel.app/api',
 });
 
 export default api;
